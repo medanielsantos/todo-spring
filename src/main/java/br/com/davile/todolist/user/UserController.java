8684cd6d2a16d2aecdc2f2fbfb63dd0eb22dab30
Daniel Henrique <medanielsantos@gmail.com>
2023-10-10 11:49:36 -0300

    implements respository and response entity

--- src/main/java/br/com/davile/todolist/user/UserController.java
+++ src/main/java/br/com/davile/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.davile.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    @Autowired
+    private IUserRepository userRepository;
 
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Resource is Exist");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("Location", "/users/" + userCreated.getId())
+                .build();
     }
 }
