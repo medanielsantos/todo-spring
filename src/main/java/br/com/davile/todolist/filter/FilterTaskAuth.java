cb97f82a6e5a16447fbed21d835fdc9d7cec7ece
Daniel Henrique <medanielsantos@gmail.com>
2023-10-12 09:28:09 -0300

    add filter, implements basic auth in tasks

---
+++ src/main/java/br/com/davile/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,56 @@
+package br.com.davile.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.davile.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var authorization = request.getHeader("Authorization");
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecodedByte = Base64.getDecoder().decode(authEncoded);
+        var authDecoded = new String(authDecodedByte);
+
+        String[] credentials = authDecoded.split(":");
+
+        String username = credentials[0];
+        String password = credentials[1];
+
+        var user = this.userRepository.findByUsername(username);
+
+        if (user == null) {
+            response.sendError(401);
+        } else {
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+            if (passwordVerify.verified) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(401);
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
